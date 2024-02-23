84c41860427c506e140f93ab566d76f137962994
dianazhoha <dianazhoga@gmail.com>
2024-02-23 15:21:00 +0200

    save

---
+++ hw1/task1.js
@@ -0,0 +1,23 @@
+let a = 'hello'
+let b = 'owu'
+let c = 'com'
+let d = 'ua'
+let n = 1
+
+console.log( a, b, c, d, n)
+let nn = 10
+let n1 = -999
+let n2 = 123
+console.log( nn, n1, n2)
+
+let n3 = 3.14
+console.log (n3)
+
+let n4 = 2.7
+console.log(n4)
+
+let n5 = 16
+let one = true
+let two = false
+
+console.log(n5, one, two)
---
+++ hw1/task2.js
@@ -0,0 +1,15 @@
+let firstName = 'Diana'
+let lastName = 'Zhoha'
+let middleName = 'Andriivna'
+ let person = ` ${firstName}  ${lastName}  ${middleName}`
+ console.log(person)
+
+let a = 100
+let b = "100"
+let c = true
+
+console.log( a, b, c)
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
