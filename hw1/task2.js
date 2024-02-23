101bd18028dff07d71b0dbbfe79ba80559dfc0f4
dianazhoha <dianazhoga@gmail.com>
2024-02-23 16:08:05 +0200

    save

--- hw1/task2.js
+++ hw1/task2.js
@@ -4,6 +4,8 @@ let middleName = 'Andriivna'
  let person = ` ${firstName}  ${lastName}  ${middleName}`
  console.log(person)
 
+
+
 let a = 100
 let b = "100"
 let c = true
@@ -13,3 +15,9 @@ console.log( a, b, c)
 console.log(typeof a)
 console.log(typeof b)
 console.log(typeof c)
+
+let fName = 'Diana'
+let lName = 'Zhoha'
+let mName = 'Andriivna'
+let space = ' '
+console.log ( fName + space + lName + space + mName)
